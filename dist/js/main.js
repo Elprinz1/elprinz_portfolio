a0998b487e901b7acb30704e810cdad812c2e0ea
Elprinz1 <coinhub.info@gmail.com>
2020-04-30 02:52:11 +0100

    'Media_Responsive'

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,8 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItem = document.querySelectorAll(".nav-item");
+const smHeading = document.querySelector(".sm-heading");
+const icons = document.querySelector(".lg-heading");
 
 // Set Initial state of Menu
 let showMenu = false;
@@ -18,15 +20,18 @@ function toggleMenu() {
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItem.forEach((item) => item.classList.add("show"));
+    smHeading.classList.add("slide-down");
+    icons.classList.add("slide-down");
 
     showMenu = true;
-
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
     navItem.forEach((item) => item.classList.remove("show"));
+    smHeading.classList.remove("slide-down");
+    icons.classList.remove("slide-down");
 
     // set menu state
     showMenu = false;
